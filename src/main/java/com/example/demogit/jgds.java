ceb6e3712aa95929a7382c309f47f155afd72444
jgds <1940138294@qq.com>
2022-01-29 06:38:01 +0800

    291   -1

--- src/main/java/com/example/demogit/jgds.java
+++ src/main/java/com/example/demogit/jgds.java
@@ -14,6 +14,7 @@ public class jgds {
         System.out.println(1);
         System.out.println(1);
         System.out.println(1);
-        System.out.println(1);
+        System.out.println(1-1);
     }
+
 }
